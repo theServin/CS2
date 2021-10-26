a6b32e6cace4b45ef09173537de0b7d9fc9c0d63
theServin <73036402+theServin@users.noreply.github.com>
2021-10-25 20:21:46 -0600

    Add files via upload

---
+++ Ch5/StockMarket.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class StockMarket{  
+  public static void main(String [] args){
+    // 1. read the file input.txt
+    // 2. for each line you read, 
+    //    2.1  create a new instance of Stock
+    //    2.2 store each instance on a data structure
+    //         e.g. an array, linked list -- a queue
+    // 3. order the data structure based on arrival time
+    //    Hint: see Comparable
+    // 4. Ask user for information (see lab)
+    // 5. Simulate data based on user's input
+    int total_time = 0; // comes from the user
+    int clock = 0;
+    while(clock <= total_time){
+      // simulation
+    }
+    // write results on results.txt
+    
+    
+  }
+  
+}
